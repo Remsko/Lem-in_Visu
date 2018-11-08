c554a704778be65555ffbe034e42b7d4bc2b3444
Remi PINOIT <rpinoit@e2r12p23.42.fr>
2018-11-08 20:29:27 +0100

    action on ant

--- srcs/draw/draw_line.c
+++ srcs/draw/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 17:14:48 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/08/17 15:49:04 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/11/08 18:45:20 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	draw_line(t_visual *v, int x1, int y1, int x2, int y2)
 	dx = x2 - x1;
 	dy = y2 - y1;
 	eps = 0;
-	SDL_SetRenderDrawColor(v->renderer, 200, 200, 200, 255);
 	while (x1 <= x2)
 	{
 		SDL_RenderDrawPoint(v->renderer, x1, y1);
--- srcs/sdl/sdl_loop.c
+++ srcs/sdl/sdl_loop.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 15:09:24 by marvin            #+#    #+#             */
-/*   Updated: 2018/11/08 18:05:08 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/11/08 20:27:22 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,31 @@ static inline float	lerp(float a, float b, float f)
     return (a + f * (b - a));
 }
 
-static int			forward_ants(t_list	*run)
+static int			forward_ants(t_list *run, float *delta_time)
 {
-	static float	delta_time = 0;
 	t_ant			*ant;
 
 	while (run != NULL)
 	{
 		ant = (t_ant *)run->content;
-		ant->actual.x = lerp(ant->prev->pos.x, ant->next->pos.x, delta_time);
-		ant->actual.y = lerp(ant->prev->pos.y, ant->next->pos.y, delta_time);
+		ant->actual.x = lerp(ant->prev->pos.x, ant->next->pos.x, *delta_time);
+		ant->actual.y = lerp(ant->prev->pos.y, ant->next->pos.y, *delta_time);
 		run = run->next;
 	}
-	delta_time += 0.005;
-	if (delta_time > 1)
+	*delta_time += 0.004;
+	if (*delta_time > 1)
 	{
-		delta_time = 0.0;
-		sdl_wait(100);
+		*delta_time = 0.0;
+		sdl_wait(200);
 	}
-	return (delta_time == 0.0 ? 1 : 0);
+	return (*delta_time == 0.0 ? 1 : 0);
 }
 
 void				sdl_loop(t_env *e, t_visual *v)
 {
+	float delta_time;
+
+	delta_time = 0;
 	e->actual_run = e->runs;
 	v->events.options.draw = TRUE;
 	while (v->events.options.exit == FALSE)
@@ -50,11 +52,37 @@ void				sdl_loop(t_env *e, t_visual *v)
 			sdl_draw(e, v);
 			if (e->actual_run != NULL)
 			{
-				if (forward_ants((t_list *)e->actual_run->content))
+				if (forward_ants((t_list *)e->actual_run->content, &delta_time))
 					e->actual_run = e->actual_run->next;
 			}
 			else
 				v->events.options.draw = FALSE;
+		}
+		if (v->events.keys[SDL_SCANCODE_RIGHT] == TRUE)
+			if (e->actual_run != NULL && e->actual_run->next != NULL)
+			{
+				e->actual_run = e->actual_run->next;
+				delta_time = 0;
+				forward_ants((t_list *)e->actual_run->content, &delta_time);
+				sdl_draw(e, v);
+				sdl_wait(200);
+			}
+		if (v->events.keys[SDL_SCANCODE_LEFT] == TRUE)
+		{
+			t_list *tmp = e->runs;
+			while (tmp->next != NULL)
+			{
+				if (tmp->next == e->actual_run)
+				{
+					e->actual_run = tmp;
+					delta_time = 0;
+					forward_ants((t_list *)e->actual_run->content, &delta_time);
+					sdl_draw(e, v);
+					sdl_wait(200);
+					break ;
+				}
+				tmp = tmp->next;
+			}
 		}
 	}
 }
